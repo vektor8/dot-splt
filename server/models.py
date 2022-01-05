67034a9d5a431229aa5a36f693a1cbf02535bbaf
vmosolea <vmosolea@bitdefender.com>
2022-01-05 03:25:28 +0200

    hope

--- server/models.py
+++ server/models.py
@@ -1,19 +1,24 @@
 import uuid
 from django.db import models
 
+
 class User(models.Model):
     name = models.CharField(max_length=50)
 
+
 class Bill(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, max_length=36)
+    id = models.CharField(
+        primary_key=True, default=uuid.uuid4, editable=False, max_length=36)
     author = models.ForeignKey(User, on_delete=models.RESTRICT)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
-    quantity = models.FloatField() 
+    quantity = models.FloatField()
     is_countable = models.BooleanField()
     bill_id = models.ForeignKey(Bill, on_delete=models.RESTRICT)
+
 
 class Payments(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
