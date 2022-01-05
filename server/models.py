a24d934f7cb3a96a109cbe2cade84cd132a0eed0
vmosolea <vmosolea@bitdefender.com>
2022-01-05 02:44:12 +0200

    add uuid to bill

--- server/models.py
+++ server/models.py
@@ -1,9 +1,11 @@
+import uuid
 from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=50)
 
 class Bill(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(User, on_delete=models.RESTRICT)
 
 class Product(models.Model):
