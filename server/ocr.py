73c532a89bc02d5d326305f022b0c83ce31699b7
vmosolea <vmosolea@bitdefender.com>
2022-01-09 15:12:53 +0200

    django vulnerability

--- server/ocr.py
+++ server/ocr.py
@@ -17,15 +17,22 @@ def img_to_bill(img: str):
     while i < len(text):
         line = text[i]
         line = line.replace(',', '.')
-        quantity, price = line.split("x")
-        is_countable = False
-        if 'BUC' in quantity:
-            is_countable = True
-            quantity = float(re.split(r"\.", quantity)[0])
-        else:
-            quantity = float(re.split(r'\s', quantity)[0])
-        price = float(price)
-        name = re.split(r'\d+[\.,]\d*', text[i + 1])[0]
-        products.append({'name' : name, 'price': price, 'quantity': quantity, 'is_countable': is_countable})
+        try:
+            quantity, price = line.split("x")
+            is_countable = False
+            if 'BUC' in quantity:
+                is_countable = True
+                quantity = float(re.split(r"\.", quantity)[0])
+            else:
+                quantity = float(re.split(r'\s', quantity)[0])
+            price = float(price)
+            name = re.split(r'\d+[\.,]\d*', text[i + 1])[0]
+            products.append({'name' : name, 'price': price, 'quantity': quantity, 'is_countable': is_countable})
+        except:
+            return products
         i += 2
     return products
+
+if __name__ == "__main__":
+    import sys
+    print(img_to_bill(sys.argv[1]))
